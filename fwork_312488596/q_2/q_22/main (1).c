32c0d3da8c7afe32a39139d859ba428e0d3771e7
Ori <bhorib@gmail.com>
2020-08-02 01:38:39 +0300

    start q2

---
+++ fwork_312488596/q_2/q_22/main (1).c
@@ -0,0 +1,22 @@
+// Server side!
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <zconf.h>
+
+
+int main(int argc, char **argv) {
+    int relevant_pid;
+    int status;
+
+    printf("The pid is: %d\n", getpid());
+//    for (j = 0; j < numSigs; j++) {
+//        sv.sival_int = sigData + j;
+//        if (sigqueue(getLong(argv[1], 0, "pid"), sig, sv) == -1)
+//            errExit("sigqueue %d", j);
+
+        return 0;
+    }
