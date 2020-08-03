5d02638baeef99b6573d6a2138930c0f20602bb6
Ori <bhorib@gmail.com>
2020-08-03 19:13:06 +0300

    add test for set_policy

---
+++ fwork_312488596/q_3/main (1).c
@@ -0,0 +1,28 @@
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int
+main(int argc, char *argv[]) {
+    int j, pol;
+    struct sched_param sp;
+
+    for (j = 1; j < argc; j++) {
+        printf("j = %d", j);
+        pol = sched_getscheduler(atoi(argv[1]));
+        if (pol == -1)
+            printf("ERROR: sched_getscheduler\n");
+
+        if (sched_getparam(atoi(argv[j]), &sp) == -1)
+            printf("ERROR: sched_getparam\\n");
+
+        printf("%s: %-5s ", argv[j],
+               (pol == SCHED_OTHER) ? "OTHER" :
+               (pol == SCHED_RR) ? "RR" :
+               (pol == SCHED_FIFO) ? "FIFO" : "???");
+        printf("%2d\n", sp.sched_priority);
+    }
+
+    exit(EXIT_SUCCESS);
+}
