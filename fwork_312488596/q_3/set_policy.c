559657b610b7bcef78be4df188241704c1c35404
bhori <bhorib@gmail.com>
2020-08-07 16:07:09 +0300

    almost fix SCHED_DEADLINE

--- fwork_312488596/q_3/set_policy.c
+++ fwork_312488596/q_3/set_policy.c
@@ -1,16 +1,18 @@
-// cat /sys/block/sda/queue/scheduler
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <sched.h>
+#include <linux/sched.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 // #define __USE_GNU
-#define SCHED_DEADLINE        6
-#define SCHED_IDLE        5
-
+// #define SCHED_DEADLINE        6
+// #define SCHED_IDLE        5
 
 void get_Policy_Priority(char status){
     int pol;
@@ -28,19 +30,47 @@ void get_Policy_Priority(char status){
             (pol == SCHED_OTHER) ? "OTHER" :
             (pol == SCHED_RR) ? "RR" :
             (pol == SCHED_FIFO) ? "FIFO" :
-            (pol == SCHED_IDLE) ? "IDLE" : "???");
+            (pol == SCHED_IDLE) ? "IDLE" :
+            (pol == SCHED_DEADLINE) ? "DEADLINE" : "???");
     }else if (status=='A'){
         printf("The policy and priority after the change: policy - %-5s, priority - ", 
             (pol == SCHED_OTHER) ? "OTHER" :
             (pol == SCHED_RR) ? "RR" :
             (pol == SCHED_FIFO) ? "FIFO" :
-            (pol == SCHED_IDLE) ? "IDLE" : "???");
+            (pol == SCHED_IDLE) ? "IDLE" :
+            (pol == SCHED_DEADLINE) ? "DEADLINE" : "???");
     }
     printf("%2d\n", sp.sched_priority);
 }
 
+
+struct sched_attr {
+    uint32_t size;
+
+    uint32_t sched_policy;
+    uint64_t sched_flags;
+
+    /* SCHED_NORMAL, SCHED_BATCH */
+    int32_t sched_nice;
+
+    /* SCHED_FIFO, SCHED_RR */
+    uint32_t sched_priority;
+
+    /* SCHED_DEADLINE (nsec) */
+    uint64_t sched_runtime;
+    uint64_t sched_deadline;
+    uint64_t sched_period;
+};
+
+int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags)
+{
+   return syscall(__NR_sched_setattr, pid, attr, flags);
+}
+
+
 int main(int argc, char **argv) {
-    int nice_value, relevant_policy, priority;
+    int relevant_policy, priority;
+    int nice_value;
     struct sched_param sp;
 
     //Check the integrity of the input
@@ -49,23 +79,6 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // int pol;
-    // struct sched_param sp2;
-
-    // pol = sched_getscheduler(0);
-    // if (pol == -1)
-    //     printf("ERROR: sched_getscheduler\n");
-
-    // if (sched_getparam(0, &sp2) == -1)
-    //     printf("ERROR: sched_getparam\\n");
-
-    // printf("The policy and priority before the change: policy - %-5s, priority - ", 
-    //         (pol == SCHED_OTHER) ? "OTHER" :
-    //         (pol == SCHED_RR) ? "RR" :
-    //         (pol == SCHED_FIFO) ? "FIFO" :
-    //         (pol == SCHED_IDLE) ? "IDLE" : "???");
-    // printf("%2d\n", sp2.sched_priority);
-
     // prints the policy and priority before the change
     get_Policy_Priority('B');
 
@@ -73,10 +86,18 @@ int main(int argc, char **argv) {
     priority = atoi(argv[2]);
     switch (relevant_policy) {
         case SCHED_DEADLINE:{
-            // printf("Schedualer of type: %d\n", relevant_policy);
-            // if (sched_setattr(0, sa, 0) == -1)
-            //     printf("ERROR! sched_setattr\n");
-            // sp.sched_priority = priority;
+            struct sched_attr attr = {
+            .size = sizeof(attr),
+            .sched_policy = SCHED_DEADLINE,
+            .sched_runtime = 30000000,
+            .sched_period = 100000000,
+            .sched_deadline = 100000000
+            };
+
+            // pid_t tid = syscall(SYS_gettid);
+
+            if (sched_setattr(0, &attr, 0))
+                perror("sched_setattr()");
             break;
         }
         case SCHED_FIFO: {
@@ -119,29 +140,15 @@ int main(int argc, char **argv) {
             printf("Wrong input, invalid policy!\n");
             return (1);
         }
-
     }
 
     // int pid=atoi(argv[3]);
 
     // if (sched_setscheduler(pid, relevant_policy, &sp) == -1)
-    if (sched_setscheduler(0, relevant_policy, &sp) == -1)
-        printf("ERROR! sched_setscheduler\n");
-
-
-    // pol = sched_getscheduler(0);
-    // if (pol == -1)
-    //     printf("ERROR: sched_getscheduler\n");
-
-    // if (sched_getparam(0, &sp2) == -1)
-    //     printf("ERROR: sched_getparam\\n");
-
-    // printf("The policy and priority after the change: policy - %-5s, priority - ", 
-    //         (pol == SCHED_OTHER) ? "OTHER" :
-    //         (pol == SCHED_RR) ? "RR" :
-    //         (pol == SCHED_FIFO) ? "FIFO" :
-    //         (pol == SCHED_IDLE) ? "IDLE" : "???");
-    // printf("%2d\n", sp2.sched_priority);
+    if(relevant_policy!=SCHED_DEADLINE){
+        if (sched_setscheduler(0, relevant_policy, &sp) == -1)
+            printf("ERROR! sched_setscheduler\n");
+    }
 
     // prints the policy and priority after the change
     get_Policy_Priority('A');
